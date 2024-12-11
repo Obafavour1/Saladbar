7d26016eaea15df8b4fdbcdd779db71f7629d2f8
Obafavour1 <olobafavour@gmail.com>
2024-12-11 10:07:15 +0000

    apos

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -8,8 +8,8 @@ const Header = () => {
       <Navbar/>
       <div className='grid  justify-center items-center  '>
           <div className=' w-[79%] md:w-[80%] mx-auto mt-10 md:mt-20 text-center '>
-            <h2 className='headerText'>London's First Dressing Focused Salad Bar</h2>
-            <p className='p1 py-8 md:w-7/12 mx-auto'>Fresh. Vibrant. Customizable. Salads Like You've Never Seen or tried, just around the corner</p>
+            <h2 className='headerText'>London&apos;s First Dressing Focused Salad Bar</h2>
+            <p className='p1 py-8 md:w-7/12 mx-auto'>Fresh. Vibrant. Customizable. Salads Like You&apos;ve Never Seen or tried, just around the corner</p>
             <div className='flex max-sm:flex-col gap-6 w-full items-center justify-center'>
               <button className='greenButton max-md:w-[150px]' type='button'>ORDER NOW</button>
               <button className='whiteButton' type="button">EXPLORE MENU</button>
