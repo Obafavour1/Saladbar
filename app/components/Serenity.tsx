640bca61015d2e317615ad88c92f0deadf68362d
Obafavour1 <olobafavour@gmail.com>
2025-05-20 20:43:18 +0100

    logo favicon

--- app/components/Serenity.tsx
+++ app/components/Serenity.tsx
@@ -7,12 +7,14 @@ const Serenity = () => {
   return (
     <section className='sections bg-[#17181A]'>
         <div className='flex max-md:flex-col md:gap-10 py-10'>
-            <Fade direction='right' delay={50} duration={500} cascade damping={1e-1}>
+            
                 <div className='relative md:w-1/2'>
-                    <Image src='/serenity.svg' alt='Salad Logo' height={360} width={549} className='max-md:w-[439px]'/>
-                    <Image src='/2024.svg' alt='Salad Logo' height={43} width={193} className='absolute right-0 bottom-4 md:w-[304px] md:h-[68px]'/> 
-                </div>
-            </Fade>
+                    <Fade direction='right' delay={50} duration={500} cascade damping={1e-1}>
+                        <Image src='/serenity.svg' alt='Salad Logo' height={360} width={549} className='max-md:w-[439px]'/>
+                        <Image src='/2024.svg' alt='Salad Logo' height={43} width={193} className='absolute right-0 bottom-4 md:w-[304px] md:h-[68px]'/> 
+                    </Fade>
+               </div>
+           
             
             <Fade direction='right' delay={50} duration={500} cascade damping={1e-1}>
                 <div className='text-white md:w-1/2'>
